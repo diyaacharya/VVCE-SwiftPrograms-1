e7a99624ac56e97dc7c3b02a0f01124ec82c01fb
Naveen Chauhan <freeclassrooms@gmail.com>
2020-10-03 22:21:08 +0530

    Swapping program added

---
+++ Swift-Experiments/Program 8.swift
@@ -0,0 +1,22 @@
+//
+//  Program.swift
+//  
+//
+//  Created by Naveen Chauhan on 03/10/20.
+//
+//MARK: - Swap Numbers Using Temporary Variable
+
+import Foundation
+
+var first:Double = 10, second:Double = 20
+
+print ("First variable value is \(first)")
+print("Second variable value is \(second)")
+
+
+var temp = first
+first = second
+print("First variable value is \(first)")
+second = temp
+print("Second variable value is \(second)")
+
